a26a9aeeaa22db815efb8fdab1d182a5aa1d77f7
duonglamh1999 <duonglamh1999@gmail.com>
2022-07-02 12:48:37 -0500

    first commit

---
+++ memory-game/script.js
@@ -0,0 +1,103 @@
+const gameContainer = document.getElementById("game");
+let card1,card2;
+let noflip=false;
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+    let front = document.createElement("div");
+    front.classList.add('card','front');
+    let back = document.createElement("div");
+    back.classList.add('card','back');
+    newDiv.append(front,back);
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+// TODO: Implement this function!
+function handleCardClick(event) {
+
+  if (noflip){return}
+  let target = event.target;
+  if (target.classList.contains('flip')){return};
+  target.style.backgroundColor= target.classList[0];
+  target.classList.add('flip')
+  if (!card1)
+  {
+    card1= this;
+  }
+  else{
+    card2=card1===this? null: this;
+    if(card1.className==card2.className)
+  {
+    card2=null;
+    card1=null;
+    console.log(card1,card2)
+  }
+  else{
+    noflip=true;
+    setTimeout(()=>{
+    card1.classList.remove('flip');
+    card2.classList.remove('flip');
+    card1.style.backgroundColor="";
+    card2.style.backgroundColor="";
+    card1=null;
+    card2=null;
+    noflip=false;
+  },1000);
+ 
+}
+}
+}
+// when the DOM loads
+createDivsForColors(shuffledColors);
+
+/* */
